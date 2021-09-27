5761f6c13cd6d08a4cef520925e20d70ab0775b0
emrancub <myself.emranhasan@gmail.com>
2021-09-27 16:24:39 +0600

    Create new user option added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-
+    @articles = Article.all
   end
 
   def about
@@ -8,9 +8,33 @@ class HomeController < ApplicationController
   end
 
   def contact
+
   end
 
   def post
 
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(set_article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  def set_article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'home#about'
   get '/post', to: 'home#post'
   get '/contact', to: 'home#contact'
+
+  post '/create', to: 'home#create'
+  get '/new', to: 'home#new'
+
+  get '/show/:id', to: 'home#show'
 end
